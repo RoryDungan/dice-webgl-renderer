ca1b98b46a36b53643762116aebc7f44b1b540ff
Rory Dungan <r_dungan@wargaming.net>
2021-09-21 09:51:52 +1000

    Move helper functions to webglUtils

--- src/index.ts
+++ src/index.ts
@@ -1,108 +1,94 @@
 import vertexShaderSource from './shaders/helloworld.vert'
 import fragmentShaderSource from './shaders/helloworld.frag'
-import { observeAndResizeCanvas } from './webglUtils'
+import { createProgramFromSources, observeAndResizeCanvas } from './webglUtils'
 
-const canvas: HTMLCanvasElement = document.querySelector('#c')
-
-const gl = canvas.getContext('webgl2')
-if (!gl) {
-  console.error("Couldn't activate webgl2 context")
-}
-
-const createShader = (
-  gl: WebGL2RenderingContext,
-  type: number,
-  source: string
-): WebGLShader => {
-  const shader = gl.createShader(type)
-  gl.shaderSource(shader, source)
-  gl.compileShader(shader)
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
-  if (success) {
-    return shader
+const main = () => {
+  // Get a WebGL 2 context
+  const canvas: HTMLCanvasElement = document.querySelector('#c')
+  const gl = canvas.getContext('webgl2')
+  if (!gl) {
+    console.error("Couldn't activate webgl2 context")
   }
 
-  console.log(gl.getShaderInfoLog(shader))
-  gl.deleteShader(shader)
-}
+  // Lint the two shaders into a program
+  const program = createProgramFromSources(gl, [
+    vertexShaderSource,
+    fragmentShaderSource,
+  ])
 
-const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
-const fragmentShader = createShader(
-  gl,
-  gl.FRAGMENT_SHADER,
-  fragmentShaderSource
-)
+  // Look up where the vertex data needs to go
+  const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
 
-const createProgram = (
-  gl: WebGL2RenderingContext,
-  vertexShader: WebGLShader,
-  fragmentShader: WebGLShader
-): WebGLProgram => {
-  const program = gl.createProgram()
-  gl.attachShader(program, vertexShader)
-  gl.attachShader(program, fragmentShader)
-  gl.linkProgram(program)
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS)
-  if (success) {
-    return program
-  }
+  // look up uniform locations
+  const resolutionUniformLocation = gl.getUniformLocation(
+    program,
+    'u_resolution'
+  )
 
-  console.log(gl.getProgramInfoLog(program))
-  gl.deleteProgram(program)
-}
+  // Create a buffer and put three 2d clip space points in it
+  const positionBuffer = gl.createBuffer()
 
-const program = createProgram(gl, vertexShader, fragmentShader)
+  // Bind it to ARRAY_BUFFER
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+  // three 2d points
+  const positions = [10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30]
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
 
-const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
-const positionBuffer = gl.createBuffer()
+  // Create a vertex array object
+  const vao = gl.createVertexArray()
 
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-
-// three 2d points
-const positions = [0, 0, 0, 0.5, 0.7, 0]
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
-
-const vao = gl.createVertexArray()
-gl.bindVertexArray(vao)
-
-gl.enableVertexAttribArray(positionAttributeLocation)
-
-const size = 2 // 2 components per iteration
-const type = gl.FLOAT // the data is 32bit floats
-const normalize = false // don't normalize the data
-const stride = 0 // 0 = move forward size * sizeof(type) each iteration to get the next position
-const offset = 0 // start at the beginning of the buffer
-gl.vertexAttribPointer(
-  positionAttributeLocation,
-  size,
-  type,
-  normalize,
-  stride,
-  offset
-)
-
-const resizer = observeAndResizeCanvas(gl.canvas, [300, 150])
-
-// resize canvas
-const drawScene = () => {
-  resizer.resizeCanvasToDisplaySize()
-
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-
-  // Clear the canvas
-  gl.clearColor(0, 0, 0, 0)
-  gl.clear(gl.COLOR_BUFFER_BIT)
-
-  // Tell it to use our program (pair of shaders)
-  gl.useProgram(program)
-
-  // Bind the attribute/buffer set we want.
+  // and make it the one we're currently working with
   gl.bindVertexArray(vao)
 
-  const primitiveType = gl.TRIANGLES
-  gl.drawArrays(primitiveType, 0, 3)
+  // turn it on
+  gl.enableVertexAttribArray(positionAttributeLocation)
+
+  // tell the attribute how to get data out of positionBuffer
+  const size = 2 // 2 components per iteration
+  const type = gl.FLOAT // the data is 32bit floats
+  const normalize = false // don't normalize the data
+  const stride = 0 // 0 = move forward size * sizeof(type) each iteration to get the next position
+  const offset = 0 // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+    positionAttributeLocation,
+    size,
+    type,
+    normalize,
+    stride,
+    offset
+  )
+
+  // resize canvas
+  const resizer = observeAndResizeCanvas(gl.canvas, [300, 150])
+
+  const drawScene = () => {
+    resizer.resizeCanvasToDisplaySize()
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program)
+
+    // Pass in the canvas resolution so we can convert from pizels to clip space in the shader
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height)
+
+    // Bind the attribute/buffer set we want.
+    gl.bindVertexArray(vao)
+
+    // draw
+    const primitiveType = gl.TRIANGLES
+    const offset = 0
+    const count = 6
+    gl.drawArrays(primitiveType, offset, count)
+
+    requestAnimationFrame(drawScene)
+  }
 
   requestAnimationFrame(drawScene)
 }
 
-requestAnimationFrame(drawScene)
+main()
--- src/webglUtils.ts
+++ src/webglUtils.ts
@@ -59,6 +59,53 @@ export const observeAndResizeCanvas = (
   return { resizeCanvasToDisplaySize }
 }
 
-type CanvasResizer = {
+export type CanvasResizer = {
   resizeCanvasToDisplaySize: () => boolean
+}
+
+const createShader = (
+  gl: WebGL2RenderingContext,
+  type: number,
+  source: string
+): WebGLShader => {
+  const shader = gl.createShader(type)
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+  if (success) {
+    return shader
+  }
+
+  console.log(gl.getShaderInfoLog(shader))
+  gl.deleteShader(shader)
+}
+
+const createProgram = (
+  gl: WebGL2RenderingContext,
+  shaders: [WebGLShader, WebGLShader]
+): WebGLProgram => {
+  const [vertexShader, fragmentShader] = shaders
+
+  const program = gl.createProgram()
+  gl.attachShader(program, vertexShader)
+  gl.attachShader(program, fragmentShader)
+  gl.linkProgram(program)
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS)
+  if (success) {
+    return program
+  }
+
+  console.log(gl.getProgramInfoLog(program))
+  gl.deleteProgram(program)
+}
+
+export const createProgramFromSources = (
+  gl: WebGL2RenderingContext,
+  sources: [string, string]
+): WebGLProgram => {
+  const [vert, frag] = sources
+  return createProgram(gl, [
+    createShader(gl, gl.VERTEX_SHADER, vert),
+    createShader(gl, gl.FRAGMENT_SHADER, frag),
+  ])
 }
